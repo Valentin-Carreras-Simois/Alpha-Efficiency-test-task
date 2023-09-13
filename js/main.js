341d8038be4e9e92c0fc6f6f3ead20d12511e79e
Valentin Simois <valentincarrerasimois@gmail.com>
2023-09-13 00:04:53 -0300

    final version

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
-const burger = document.querySelector(".burger");
-const navMenu = document.querySelector(".nav-menu");
 
-burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n.
-    addEventListener("click", () => {
-    burger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
 
 const countdown = () =>{
     const countDate = new Date('November 28, 2023 00:00:00').getTime();
